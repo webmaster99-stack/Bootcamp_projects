7028be106906a066e9e6db0d6fef4f4e170a2802
Ilian Hadzhidimitrov <ilian_hadzhidimitrov99@abv.bg>
2024-10-16 17:35:49 +0300

    Added Quiz Game project

---
+++ Quizz Game/data.py
@@ -0,0 +1,39 @@
+question_data = [
+    {
+     "text": "Linus Torvalds created Linux and Git.",
+     "answer": "True"
+     },
+    {
+     "text": "Time on Computers is measured via the EPOX System.",
+     "answer": "False"
+     },
+    {
+     "text": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+     "answer": "True"},
+    {
+     "text": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+     "answer": "True"},
+    {
+     "text": "RAM stands for Random Access Memory.",
+     "answer": "True"
+     },
+    {
+     "text": "Linux was first created as an alternative to Windows XP.",
+     "answer": "False"
+     },
+    {
+     "text": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+     "answer": "False"
+     },
+    {
+     "text": "The Windows 7 operating system has six main editions.",
+     "answer": "True",
+     },
+    {
+     "text": "The logo for Snapchat is a Bell.",
+     "answer": "False",
+     },
+    {
+     "text": "&quot;HTML&quot; stands for Hypertext Markup Language.",
+     "answer": "True"
+     }]
