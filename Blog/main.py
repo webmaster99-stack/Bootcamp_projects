5e479588d579b3c66a7a72bdfcd5de34472d2c5d
Ilian Hadzhidimitrov <ilian_hadzhidimitrov99@abv.bg>
2024-11-15 19:32:55 +0200

    Added styling to Blog project

--- Blog/main.py
+++ Blog/main.py
@@ -1,29 +1,35 @@
 from flask import Flask, render_template
 import requests
-from post import Post
 
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
-response = requests.get("https://api.npoint.io/0219386e8315162c70b2")
-posts = response.json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_objects.append(post_obj)
 
 @app.route('/')
-def get_all_post():
-    return render_template("index.html", all_posts=post_objects)
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- Blog/post.py
+++
@@ -1,7 +0,0 @@
-class Post:
-
-    def __init__(self, id, title, subtitle, body):
-        self.id = id
-        self.title = title
-        self.subtitle = subtitle
-        self.body = body
