ff1ff73557e6dcf375c721dbd91809a794adbcaa
Ilian Hadzhidimitrov <ilian_hadzhidimitrov99@abv.bg>
2024-11-16 09:07:07 +0200

    Added form functionality to contact.html

--- Blog/main.py
+++ Blog/main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+from dotenv import load_dotenv
 
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+load_dotenv()
 
 app = Flask(__name__)
 
+my_email = os.environ["EMAIL_USERNAME"]
+my_password = os.environ["EMAIL_PASSWORD"]
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 @app.route('/')
 def get_all_posts():
@@ -17,9 +23,13 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/post/<int:index>")
@@ -31,5 +41,13 @@ def show_post(index):
     return render_template("post.html", post=requested_post)
 
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject: New message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=email_message)
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
